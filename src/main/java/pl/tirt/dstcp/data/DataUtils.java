a7a7944cce7f386f8ce6750312734171ff70bfd3
Artur Waliczek <artur.waliczek@capgemini.com>
2017-04-15 16:11:08 +0200

    DataUtils added

---
+++ src/main/java/pl/tirt/dstcp/data/DataUtils.java
@@ -0,0 +1,11 @@
+package pl.tirt.dstcp.data;
+
+/**
+ * Created by AWALICZE on 15.04.2017.
+ */
+public class DataUtils {
+
+    public static String DIRECTORY_PATH = "C:\\tirt\\";
+    public static String FILE_NAME = "tcpdump.txt";
+    public static String FILE_REGEX = " ";
+}
--- src/main/java/pl/tirt/dstcp/data/service/DataProvider.java
+++ src/main/java/pl/tirt/dstcp/data/service/DataProvider.java
@@ -1,5 +1,7 @@
 package pl.tirt.dstcp.data.service;
 
+import pl.tirt.dstcp.data.DataUtils;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -9,9 +11,6 @@ import java.util.Scanner;
  * Created by AWALICZE on 14.04.2017.
  */
 public class DataProvider {
-
-    private String FILE_PATH = "C:\\tirt\\tcpdump.txt";
-    private String FILE_REGEX = " ";
 
     private static DataProvider instance;
 
@@ -23,7 +22,7 @@ public class DataProvider {
     }
 
     public ArrayList<String[]> getData() {
-        File dataSource = getDataSource(FILE_PATH);
+        File dataSource = getDataSource(DataUtils.DIRECTORY_PATH + DataUtils.FILE_NAME);
 
         Scanner scanner = null;
         try {
@@ -44,10 +43,10 @@ public class DataProvider {
     }
 
     private ArrayList<String[]> processData(Scanner scanner){
-        ArrayList<String[]> data = new ArrayList<>();
+        ArrayList<String[]> data = new ArrayList<String[]>();
         while (scanner.hasNextLine()){
             String line = scanner.nextLine();
-            data.add(line.split(FILE_REGEX));
+            data.add(line.split(DataUtils.FILE_REGEX));
         }
         return data;
     }
