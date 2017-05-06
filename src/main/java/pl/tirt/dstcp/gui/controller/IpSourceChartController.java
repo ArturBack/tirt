a71085fd52174837292e16a9a574db4bb2e3389f
Artur Waliczek <artur.waliczek@gmail.com>
2017-05-06 13:52:55 +0200

    IpSourceChart changed

--- src/main/java/pl/tirt/dstcp/gui/controller/IpSourceChartController.java
+++ src/main/java/pl/tirt/dstcp/gui/controller/IpSourceChartController.java
@@ -1,11 +1,16 @@
 package pl.tirt.dstcp.gui.controller;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.LineChart;
 import javafx.scene.chart.StackedBarChart;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.ComboBox;
+import pl.tirt.dstcp.data.model.BitsInPacketInfo;
 import pl.tirt.dstcp.data.model.IpProtocolVersionInPacketInfo;
 import pl.tirt.dstcp.data.repositories.IpProtocolVersionRepository;
 import pl.tirt.dstcp.gui.utils.TimestampType;
@@ -21,38 +26,106 @@ import java.util.List;
 public class IpSourceChartController {
 
     @FXML
-    private StackedBarChart<String, Integer> ipSourceChart;
+    private LineChart<String, Integer> ipSourceChart;
 
     @FXML
-    private CategoryAxis xAxis;
+    private ComboBox<String> scale_type_combo;
+
+    @FXML
+    private ComboBox<String> ip_source_combo;
 
     private HashMap<String, ObservableList<Integer>> ipSourcesMap = new HashMap<>();
-    private ObservableList<Integer> timeValues = FXCollections.observableArrayList();
-
+    private ObservableList<Integer> timeValues;
+    private List<IpProtocolVersionInPacketInfo> data;
     private TimestampType timestampType = TimestampType.SECOND;
-
 
     @FXML
     private void initialize() {
-        List<IpProtocolVersionInPacketInfo> data = IpProtocolVersionRepository.getInstance().getData();
+        data = IpProtocolVersionRepository.getInstance().getData();
+        setData(data);
+        initScaleTypes();
+    }
+
+    private void setData(List<IpProtocolVersionInPacketInfo> data){
+        timeValues = TimestampUtils.createTimeValues(timestampType,getTimestamps(data));
+        fillIpSourceMap(data);
+        fillIpSourcesCombo();
+        fillChartWithData();
+    }
+
+    private void initScaleTypes() {
+        String[] options = {TimestampType.SECOND.name(), TimestampType.MILISECOND.name()};
+        scale_type_combo.getItems().addAll(options);
+        //default value
+        scale_type_combo.getSelectionModel().select(0);
+
+        scale_type_combo.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                if(newValue.equals(TimestampType.SECOND.name())){
+                    setTimestampType(TimestampType.SECOND);
+                    reloadData();
+
+                } else if(newValue.equals(TimestampType.MILISECOND.name())){
+                    setTimestampType(TimestampType.MILISECOND);
+                    reloadData();
+                }
+            }
+        });
+    }
+
+    private void fillIpSourcesCombo() {
+        ip_source_combo.getItems().addAll(ipSourcesMap.keySet());
+        //default value
+        ip_source_combo.getSelectionModel().select(0);
+
+        ip_source_combo.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                changeShownIp();
+            }
+        });
+    }
+
+    private void reloadData() {
+        ipSourceChart.getData().clear();
         timeValues = TimestampUtils.createTimeValues(timestampType,getTimestamps(data));
         fillIpSourceMap(data);
         fillChartWithData();
     }
 
     private void fillChartWithData() {
-        for(String key: ipSourcesMap.keySet()) {
-            XYChart.Series<String, Integer> series = new XYChart.Series<>();
-            series.setName(key);
-            for (int i = 0; i < timeValues.size(); i++) {
-                XYChart.Data data = new XYChart.Data(timeValues.get(i).toString(), ipSourcesMap.get(key).get(i));
-                series.getData().add(data);
-            }
-            ipSourceChart.getData().add(series);
+        switch(timestampType){
+            case SECOND:
+                fillChartWithProperData(0);
+                break;
+            case MILISECOND:
+                fillChartWithProperData(TimestampUtils.getStartIndexOfMiliTimeValues(timeValues));
+                break;
+            default:
         }
     }
 
+    private void changeShownIp() {
+        ipSourceChart.getData().clear();
+        fillChartWithData();
+    }
+
+    public void setTimestampType(TimestampType timestampType) {
+        this.timestampType = timestampType;
+    }
+
+    private void fillChartWithProperData(int startIndex) {
+        String selectedItem = ip_source_combo.getSelectionModel().getSelectedItem();
+        XYChart.Series series = new XYChart.Series();
+        for(int i = startIndex; i < timeValues.size(); i++){
+            series.getData().add(new XYChart.Data(timeValues.get(i).toString(), ipSourcesMap.get(selectedItem).get(i)));
+        }
+        ipSourceChart.getData().add(series);
+    }
+
     private void fillIpSourceMap(List<IpProtocolVersionInPacketInfo> data) {
+        ipSourcesMap.clear();
         for(IpProtocolVersionInPacketInfo info : data){
             Integer time = TimestampUtils.getTime(timestampType,info.getTimestamp());
 
